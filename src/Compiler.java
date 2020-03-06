934fd51ffd0ea24abe0335222558d9a638c10a4f
NASR Alexis <nasr@L-054011F108-08.salsa.univ-amu.fr>
2020-03-06 17:39:28 +0100

    corrigé un bug improbable dans Compiler.java, le pushBackReader doit être défini avec une taille de buffer explicite (par exemple 1024) sinon, la taille du buffer peut ne pas être suffisante pour le lexer (c'est un peu approximatif, je n'ai pas creusé plus loin)

--- src/Compiler.java
+++ src/Compiler.java
@@ -16,7 +16,7 @@ public class Compiler
 	String baseName = null;
 	try {
 	    if (0 < args.length) {
-		br = new PushbackReader(new FileReader(args[0]));
+		br = new PushbackReader(new FileReader(args[0]), 1024);
 		baseName = removeSuffix(args[0], ".l");
 	    }
 	    else{
